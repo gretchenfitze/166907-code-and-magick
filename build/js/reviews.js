95dc105e4b9fa77b723b7d6be448dee5811b87c8
gretchenfitze <gretchenfitze@gmail.com>
2016-04-20 23:44:09 +0300

    Выделен модуль Rendering reviews

--- build/js/reviews.js
+++ build/js/reviews.js
@@ -2,21 +2,11 @@
 
 (function() {
   var utilities = require('../../src/utilities');
+  var renderingReviews = require('./reviews-render');
 
   var filtersContainer = document.querySelector('.reviews-filter');
   var reviewsContainer = document.querySelector('.reviews-list');
-  var templateElement = document.getElementById('review-template');
-  var elementToClone;
   var reviewsBlock = document.querySelector('.reviews');
-
-  if ('content' in templateElement) {
-    elementToClone = templateElement.content.querySelector('.review');
-  } else {
-    elementToClone = templateElement.querySelector('.review');
-  }
-
-  /** @constant {number} */
-  var IMAGE_LOAD_TIMEOUT = 10000;
 
   /** @constant {string} */
   var REVIEWS_LOAD_URL = '//o0.github.io/assets/json/reviews.json';
@@ -42,48 +32,6 @@
     'POPULAR': 'reviews-popular'
   };
 
-  /**
-   * @param {Object} data
-   * @param {HTMLElement} container
-   * @return {HTMLElement}
-   */
-  var getReviewElement = function(data, container) {
-    var element = elementToClone.cloneNode(true);
-    var author = data.author;
-    element.querySelector('.review-rating').textContent = data.rating;
-
-    // Исправление положения звёздочки рейтинга
-    element.querySelector('.review-rating').style.backgroundPositionX = '10px';
-    element.querySelector('.review-rating').style.backgroundRepeatX = 'no-repeat';
-    element.querySelector('.review-rating').style.width = '40px';
-
-    element.querySelector('.review-text').textContent = data.description;
-    container.appendChild(element);
-
-    var authorPicture = new Image();
-    var pictureLoadTimeout;
-
-    authorPicture.onload = function() {
-      clearTimeout(pictureLoadTimeout);
-      element.querySelector('img').setAttribute('src', author.picture);
-      element.querySelector('img').setAttribute('width', '124px');
-      element.querySelector('img').setAttribute('height', '124px');
-    };
-
-    authorPicture.onerror = function() {
-      element.classList.add('review-load-failure');
-    };
-
-    authorPicture.src = author.picture;
-
-    pictureLoadTimeout = setTimeout(function() {
-      authorPicture.src = '';
-      element.classList.add('review-load-failure');
-    }, IMAGE_LOAD_TIMEOUT);
-
-    return element;
-  };
-
   /** @param {Array.<Object>} reviews */
   var renderReviews = function(reviewsToRender, page, replace) {
     if (replace) {
@@ -94,7 +42,7 @@
     var to = from + PAGE_SIZE;
 
     reviewsToRender.slice(from, to).forEach(function(review) {
-      getReviewElement(review, reviewsContainer);
+      renderingReviews.getReviewElement(review, reviewsContainer);
     });
     if (to < reviewsToRender.length) {
       utilities.showElement(showMoreReviews);
